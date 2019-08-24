482a419be5f8fe28a6064d39765a9d19c559ef48
OneGoodReas0n <korobovi4@gmail.com>
2019-08-24 15:29:51 +0300

    Implementing Home, Main pages

--- src/index.js
+++ src/index.js
@@ -3,10 +3,22 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+import '../node_modules/bootstrap/dist/css/bootstrap.min.css'
+import { BrowserRouter } from 'react-router-dom'
+import { createStore, applyMiddleware } from "redux";
+import { Provider } from 'react-redux'
+import thunk from 'redux-thunk'
+import reducers from './reducers'
+
+const store = createStore(reducers, applyMiddleware(thunk))
 
 ReactDOM.render(
-
-    <App />, document.getElementById('root'));
+    <Provider store={store}>
+        <BrowserRouter>
+            <App />
+        </BrowserRouter>
+    </Provider>
+    , document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
